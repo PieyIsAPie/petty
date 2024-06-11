2850e4b58bff51b34e6c6972605c556fc49edadd
PieyIsAPie <foreynator.serverowner@gmail.com>
2024-06-11 19:38:21 -0400

    small patch

--- main.py
+++ main.py
@@ -5,23 +5,24 @@ import requests
 bot = discord.Bot(intents=discord.Intents.all())
 
 SCRIPTDIR = os.path.dirname(os.path.realpath(__file__))
+TOKEN = os.getenv("TOKEN")
 
 #Who used the weld tool on my fuckin cat?
-@bot.slash_command(guild_ids=[790642397373005855, 1085739941658628147], name="kitty", description="ooo a kitty! I LOVE KITTY!")
+@bot.slash_command(name="kitty", description="ooo a kitty! I LOVE KITTY!")
 async def kitty(ctx):
     response = requests.get("https://api.thecatapi.com/v1/images/search")
     data = response.json()
     image_url = data[0]['url']
     await ctx.respond(image_url)
 
-@bot.slash_command(guild_ids=[790642397373005855, 1085739941658628147], name="ducky", description="DUCK DUCK I LOVE DUCKS ASADSDSADASDSADAJAV")
+@bot.slash_command(name="ducky", description="DUCK DUCK I LOVE DUCKS ASADSDSADASDSADAJAV")
 async def ducky(ctx):
     response = requests.get("https://random-d.uk/api/v1/random")
     data = response.json()
     image_url = data['url']
     await ctx.respond(image_url)
 
-@bot.slash_command(guild_ids=[790642397373005855, 1085739941658628147], name="doggie", description="I LOVE DOGS!!!")
+@bot.slash_command(name="doggie", description="I LOVE DOGS!!!")
 async def doggy(ctx):
     response = requests.get("https://dog.ceo/api/breeds/image/random")
     data = response.json()
@@ -41,4 +42,4 @@ async def on_ready():
     await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="the ducks"))
     #bot.process_application_commands()
 
-bot.run(open("token.txt", "r").read())
+bot.run(TOKEN)
